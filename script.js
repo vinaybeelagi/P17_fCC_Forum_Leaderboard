c8ae0ed50527d8d33f62b1ba0c0dc7452b2611e1
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-25 16:51:17 +0530

    API assigned to variables

---
+++ script.js
@@ -0,0 +1,18 @@
+const forumLatest = "https://forum-proxy.freecodecamp.rocks/latest";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+const postsContainer = document.getElementById("posts-container");
+
+const allCategories = {
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback" },
+  417: { category: "freeCodeCamp Support", className: "support" },
+  421: { category: "JavaScript", className: "javascript" },
+  423: { category: "HTML - CSS", className: "html-css" },
+  424: { category: "Python", className: "python" },
+  432: { category: "You Can Do This!", className: "motivation" },
+  560: { category: "Backend Development", className: "backend" },
+};
+
